31a7bb4fb6d354e40345d2ed5eea503573659f1e
grscheller <geoffrey@scheller.com>
2026-02-07 14:02:01 -0500

    Finished boring-math docstring homogenization

--- src/boring_math/number_theory/__init__.py
+++ src/boring_math/number_theory/__init__.py
@@ -12,9 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Number Theory Library
+"""
+Number Theory Library
+---------------------
 
-Collection of integer related functions useful in number theory.
+.. admonition:: Elementary Number Theory
+
+    Collection of integer related functions useful in number theory.
 
 """
 
